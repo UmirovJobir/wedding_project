c404a666a8e9e5f1ac0d117a44d2137e7754a34c
Jahongir Umirov <jahongirumirov@Jahongirs-MacBook-Pro.local>
2022-07-14 16:29:07 +0500

    action_16

--- accounts/models.py
+++ accounts/models.py
@@ -3,8 +3,43 @@ from django.contrib.auth.models import  AbstractUser
 
 
 class UserModel(AbstractUser):
+
+    TOSHKENT = "Toshkent"
+    TOSHKENT_V = "Toshkent_v"
+    ANDIJON = "Andijon "
+    BUXORO = "Buxoro"
+    FARGONA = "Farg'ona"
+    SIRDARYO = "Sirdaryo"
+    JIZZAX = "Jizzax"
+    NAMANGAN = "Namangan"
+    NAVOIY = "Navoiy"
+    QORAQAL = "Qoraqalpog'iston Respublikasi"
+    SAMARQAND = "Samarqand"
+    SURXONDARYO = "Surxondaryo"
+    XORAZM = "Xorazm"
+    QASHQADARYO = "Qashqadaryo"
+
+    CITY = (
+        (TOSHKENT, "Toshkent"),
+        (TOSHKENT_V, "Toshkent_v"),
+        (ANDIJON, "Andijon"),
+        (BUXORO, "Buxoro"),
+        (FARGONA, "Farg'ona"),
+        (SIRDARYO, "Sirdaryo"),
+        (JIZZAX, "Jizzax"),
+        (NAMANGAN, "Namangan"),
+        (NAVOIY, "Navoiy"),
+        (QORAQAL, "Qoraqalpog'iston Respublikasi"),
+        (SAMARQAND, "Samarqand"),
+        (SURXONDARYO, "Surxondaryo"),
+        (XORAZM, "Xorazm"),
+        (QASHQADARYO, "Qashqadaryo"),
+
+
+    )
+
     number = models.CharField(max_length=9)
-    city = models.CharField(max_length=20)
+    city = models.CharField(max_length=20, choices=CITY)
     event_date = models.DateField(blank=True, null=True)
 
     class Meta:
