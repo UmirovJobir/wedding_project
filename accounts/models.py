c52f935f55d1908424ea98c10113a0338bcdeb10
Jahongir Umirov <jahongirumirov@Jahongirs-MacBook-Pro.local>
2022-07-15 11:46:52 +0500

    action_25

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import  AbstractUser
+from django.core.exceptions import ValidationError
 
+def validate_length(value,length=9):
+    if value.isdigit():
+        if len(str(value))!=length:
+            raise ValidationError(f"Ошибка: Номер должен содержать {length} цифр.")
+    else:
+        raise ValidationError("Ошибка: Пишите только цифры.")
 
 class UserModel(AbstractUser):
 
@@ -37,19 +44,16 @@ class UserModel(AbstractUser):
 
 
     )
-
-    number = models.CharField(max_length=9)
+    
+    username = models.CharField(max_length=30)
+    number = models.CharField(max_length=9, unique=True, validators=[validate_length])
     city = models.CharField(max_length=50, choices=CITY)
     event_date = models.DateField(blank=True, null=True)
 
     class Meta:
         ordering = ['-date_joined']
+        verbose_name_plural = "Пользователи"
     
-    def save(self, *args, **kwargs):
-        super(UserModel, self).save(*args, **kwargs)
-    
-    class Meta:
-         verbose_name_plural = "Пользователи"
 
 
  
