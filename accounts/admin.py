32c9802734af9b908e367fa6d44ec423fb8d1ce3
Jahongir Umirov <jahongirumirov@Jahongirs-MacBook-Pro.local>
2022-07-16 11:56:07 +0500

    action_33

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import UserModel
+from .models import UserModel, BlacklistUser
 
 
 @admin.register(UserModel)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['username', 'number']
+    list_filter = ['username', 'number']
+    search_fields = ['username', 'number']
+
+@admin.register(UserModel)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'number', 'reason']
     list_filter = ['username', 'number']
     search_fields = ['username', 'number']
