53945e1515c71044ea95bef644b5190826b0aae4
Jahongir Umirov <jahongirumirov@Jahongirs-MacBook-Pro.local>
2022-07-16 22:08:50 +0500

    action_73

--- wedding/settings.py
+++ wedding/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
  
     'nested_admin',
     'drf_yasg',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -62,7 +63,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'wedding.urls'
 
