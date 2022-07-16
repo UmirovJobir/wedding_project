622d1483e2ed234cd996ee32fa889a7f17c03063
Jahongir Umirov <jahongirumirov@Jahongirs-MacBook-Pro.local>
2022-07-16 17:48:40 +0500

    action_55

--- wedding/urls.py
+++ wedding/urls.py
@@ -37,4 +37,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('services/', include('services.urls')),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-docs'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc-docs')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
