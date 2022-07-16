df1f887d3d979d007850eb26bf82632bb29d41f6
Jahongir Umirov <jahongirumirov@Jahongirs-MacBook-Pro.local>
2022-07-17 00:06:18 +0500

    action_83

--- wedding/settings.py
+++ wedding/settings.py
@@ -66,11 +66,8 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-# CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-  'http://localhost:2000',
-)
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 ROOT_URLCONF = 'wedding.urls'
 
